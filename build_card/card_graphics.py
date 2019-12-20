39d72a03aa73141f206469afa475cf948555fc0e
James Rocker <james.rocker@simplybusiness.co.uk>
2019-12-20 15:28:55 +0000

    made as a module and run using slightly more pythonic method

--- build_card/card_graphics.py
+++ build_card/card_graphics.py
@@ -1,70 +1,72 @@
 import turtle
 
-screen = turtle.Screen()
-screen.setup(900, 900)
 
-circle = turtle.Turtle()
-circle.shape('circle')
-circle.color('red')
-circle.speed('fastest')
-circle.up()
+def build_card():
+    screen = turtle.Screen()
+    screen.setup(900, 900)
 
-square = turtle.Turtle()
-square.shape('square')
-square.color('green')
-square.speed('fastest')
-square.up()
+    circle = turtle.Turtle()
+    circle.shape('circle')
+    circle.color('red')
+    circle.speed('fastest')
+    circle.up()
 
-circle.goto(0, 280)
-circle.stamp()
+    square = turtle.Turtle()
+    square.shape('square')
+    square.color('green')
+    square.speed('fastest')
+    square.up()
 
-k = 0
-for i in range(1, 17):
-    y = 30 * i
-    for j in range(i - k):
-        x = 30 * j
-        square.goto(x, -y + 280)
-        square.stamp()
-        square.goto(-x, -y + 280)
-        square.stamp()
+    circle.goto(0, 280)
+    circle.stamp()
 
-    if i % 4 == 0:
-        x = 30 * (j + 1)
-        circle.color('red')
-        circle.goto(-x, -y + 280)
-        circle.stamp()
-        circle.goto(x, -y + 280)
-        circle.stamp()
-        k += 2
+    k = 0
+    for i in range(1, 17):
+        y = 30 * i
+        for j in range(i - k):
+            x = 30 * j
+            square.goto(x, -y + 280)
+            square.stamp()
+            square.goto(-x, -y + 280)
+            square.stamp()
 
-    if i % 4 == 3:
-        x = 30 * (j + 1)
-        circle.color('yellow')
-        circle.goto(-x, -y + 280)
-        circle.stamp()
-        circle.goto(x, -y + 280)
-        circle.stamp()
+        if i % 4 == 0:
+            x = 30 * (j + 1)
+            circle.color('red')
+            circle.goto(-x, -y + 280)
+            circle.stamp()
+            circle.goto(x, -y + 280)
+            circle.stamp()
+            k += 2
 
-square.color('brown')
-for i in range(17, 20):
-    y = 30 * i
-    for j in range(3):
-        x = 30 * j
-        square.goto(x, -y + 280)
-        square.stamp()
-        square.goto(-x, -y + 280)
-        square.stamp()
+        if i % 4 == 3:
+            x = 30 * (j + 1)
+            circle.color('yellow')
+            circle.goto(-x, -y + 280)
+            circle.stamp()
+            circle.goto(x, -y + 280)
+            circle.stamp()
 
-
-text = turtle.Turtle()
-text.penup()
-style = ('Courier', 30)
-text.speed('fastest')
-text.setpos(0, 350)
-text.color('black')
-text.pendown()
-text.write('Happy Holidays!', font=style, align='center')
-text.hideturtle()
+    square.color('brown')
+    for i in range(17, 20):
+        y = 30 * i
+        for j in range(3):
+            x = 30 * j
+            square.goto(x, -y + 280)
+            square.stamp()
+            square.goto(-x, -y + 280)
+            square.stamp()
 
 
-turtle.exitonclick()
+    text = turtle.Turtle()
+    text.penup()
+    style = ('Courier', 30)
+    text.speed('fastest')
+    text.setpos(0, 350)
+    text.color('black')
+    text.pendown()
+    text.write('Happy Holidays!', font=style, align='center')
+    text.hideturtle()
+
+
+    turtle.exitonclick()
---
+++ run.py
@@ -0,0 +1,4 @@
+from build_card import card_graphics as cg
+
+if __name__ == "__main__":
+    cg.build_card()
